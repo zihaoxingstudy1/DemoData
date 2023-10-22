c1e18596e3f00c6b8443e6c220fa5b0786be1bbd
ZihaoXing <zihaoxing@stu2019.jnu.edu.cn>
2023-10-22 20:19:27 +0800

    add data
    add data

---
+++ R/hello_demo.R
@@ -0,0 +1,10 @@
+#' @title 简单的hello demo data函数
+#' @description 简单的hello demo data函数
+#' @param 
+#' @return 'hello demo data'
+#' @export
+#' @examples
+#' hello_demo()
+hello_demo  <- function( ){
+    print( "hello demo data")
+}
